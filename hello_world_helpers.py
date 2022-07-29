2e06d2e1e3876209a91b1dd7dda6299db37f2cf4
Preston Martin <pmartindev@github.com>
2022-07-29 18:23:02 -0500

    Add logging functions.

---
+++ hello_world_helpers.py
@@ -0,0 +1,12 @@
+"""
+Helper module for the hello world application. 
+"""
+
+import logging
+
+def log(message):
+    """
+    Logs a message to the console.
+    """
+    logging.info(message)
+    print(message)
